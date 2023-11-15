a51e316f979e555f0b545edc765b91f038903b89
Alexei Prokudin <prokudin@jlab.org>
2023-11-15 13:30:56 -0500

    Update jar.py

--- analysis/corelib/jar.py
+++ analysis/corelib/jar.py
@@ -9,8 +9,8 @@ import copy
 #--from tools
 from tools.tools     import load,save,checkdir,lprint
 from tools.config    import conf,load_config
-from tools.inputmod  import INPUTMOD
-from tools.randomstr import id_generator
+#from tools.inputmod  import INPUTMOD
+#from tools.randomstr import id_generator
 
 #--from local
 from analysis.corelib import core
